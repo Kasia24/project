1e9bacbe9fc2fb0b4edc63e680f17a463dc3c3d1
Kasia24 <160325853+Kasia24@users.noreply.github.com>
2024-10-12 09:37:09 +0200

    add commit

--- script.js
+++ script.js
@@ -1,36 +1,32 @@
-const apiKey = "2fd9551be199200f928abc93ae4bceb1"; // Wstaw swój klucz API
+const apiKey = "2fd9551be199200f928abc93ae4bceb1"; // Wstaw klucz API
+const url = `https://api.themoviedb.org/3/trending/movie/week?api_key=${apiKey}`;
 
-// Funkcja do wyszukiwania filmów
-async function searchMovies() {
-  const query = document.getElementById("movies-list").value;
-  const url = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${query}`;
+fetch(url)
+  .then((response) => response.json())
+  .then((data) => {
+    // Call a function to render movies on the screen
+    renderMovies(data.results);
+  })
+  .catch((error) => console.error("Error fetching movies:", error));
 
-  try {
-    const response = await fetch(url);
-    const data = await response.json();
-    displayMovies(data.results);
-  } catch (error) {
-    console.error("Error fetching data from TMDb:", error);
-  }
-}
-// Funkcja do wyświetlania wyników wyszukiwania
-function displayMovies(movies) {
-  const moviesList = document.getElementById("movies-card");
-  moviesList.innerHTML = "";
-
-  if (movies.length === 0) {
-    moviesList.innerHTML = "<p>No movies found</p>";
-    return;
-  }
+function renderMovies(movies) {
+  const container = document.getElementById("movies-container"); // A div with id movies-container
 
   movies.forEach((movie) => {
-    const movieItem = document.createElement("div");
-    movieItem.classList.add("movie-item");
-    movieItem.innerHTML = `
-            <h2>${movie.title}</h2>
-            <p>${movie.overview}</p>
-            <p><strong>Release date:</strong> ${movie.release_date}</p>
+    const movieCard = document.createElement("div");
+    movieCard.classList.add("movies-card");
+
+    movieCard.innerHTML = `
+            <img src="https://image.tmdb.org/t/p/w500${
+              movie.poster_path
+            }" alt="${movie.title}">
+            <h3>${movie.title}</h3>
+            <p>${movie.genre_ids.slice(0, 2).join(", ")} | ${
+      movie.release_date.split("-")[0]
+    }</p>
+            <div class="rating">⭐⭐⭐⭐⭐</div>
         `;
-    moviesList.appendChild(movieItem);
+
+    container.appendChild(movieCard);
   });
 }
