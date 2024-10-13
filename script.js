a34e678249395fc5d9ace7307139943e9d9b4334
Kasia24 <160325853+Kasia24@users.noreply.github.com>
2024-10-13 09:34:04 +0200

    add

--- script.js
+++ script.js
@@ -1,60 +1,45 @@
 const API_KEY = "2fd9551be199200f928abc93ae4bceb1"; // Wstaw swój API key tutaj
-const API_URL = `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=pl-PL&page=1`;
+// script.js
+
 let page = 1;
+const moviesContainer = document.getElementById("movies");
 const loadMoreButton = document.getElementById("load-more");
-// Funkcja pobierająca popularne filmy
-async function fetchMovies() {
-  const response = await fetch(API_URL);
-  const data = await response.json();
-  displayMovies(data.results);
+
+// Funkcja do pobierania filmów z TMDb API
+function getMovies(page) {
+  const url = `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=pl-PL&page=${page}`;
+
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      displayMovies(data.results);
+    })
+    .catch((error) => console.log(error));
 }
 
-// Funkcja wyświetlająca filmy
+// Funkcja do wyświetlania filmów w HTML
 function displayMovies(movies) {
-  const movieContainer = document.getElementById("movies");
-  movieContainer.innerHTML = ""; // Czyszczenie zawartości
-
   movies.forEach((movie) => {
     const movieCard = document.createElement("div");
     movieCard.classList.add("movie-card");
 
     movieCard.innerHTML = `
-            <img src="https://image.tmdb.org/t/p/w500${
-              movie.poster_path
-            }" alt="${movie.title}">
-            <div class="movie-title">${movie.title}</div>
-            <div class="movie-info">Drama | ${
-              movie.release_date.split("-")[0]
-            }</div>
-            <div class="stars">${getStars(movie.vote_average)}</div>
+            <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="${movie.title}">
+            <div class="movie-info">
+                <h2 class="movie-title">${movie.title}</h2>
+                <p>${movie.release_date}</p>
+            </div>
         `;
 
-    movieContainer.appendChild(movieCard);
+    moviesContainer.appendChild(movieCard);
   });
 }
-
-// Funkcja generująca gwiazdki na podstawie oceny
-function getStars(vote) {
-  const fullStars = Math.floor(vote / 2);
-  let stars = "";
-  for (let i = 0; i < 5; i++) {
-    if (i < fullStars) {
-      stars += "&#9733;"; // Pełna gwiazdka
-    } else {
-      stars += "&#9734;"; // Pusta gwiazdka
-    }
-  }
-  return stars;
-}
-
-// Wywołanie funkcji
-fetchMovies();
 
 // Obsługa przycisku "Załaduj więcej"
 loadMoreButton.addEventListener("click", () => {
   page++;
-  fetchMovies(page);
+  getMovies(page);
 });
 
 // Pobranie początkowej listy filmów
-fetchMovies(page);
+getMovies(page);
